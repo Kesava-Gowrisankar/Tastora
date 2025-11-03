13f5133bf3cf588097b3c921f220274da89c1bdd
kesav <kesava@testpress.com>
2025-11-03 14:29:10 +0530

    Start Add recipe

--- recipe/urls.py
+++ recipe/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 app_name='recipe'
 urlpatterns=[
-    path('',views.HomePage.as_view(),name='home'),
+    path('home/',views.HomePage.as_view(),name='home'),
+    path('recipes/',views.RecipePage.as_view(),name='recipes'),
+    path('recipe/<int:pk>/', views.RecipeDetailView.as_view(), name='recipe_detail'),
 ]
