56f92cec4ecf56ef9d2f076de9e86e06d7a73927
kesav <kesava@testpress.com>
2025-11-03 14:29:10 +0530

    Start Add recipe

--- recipe/urls.py
+++ recipe/urls.py
@@ -3,4 +3,6 @@ from . import views
 app_name='recipe'
 urlpatterns=[
     path('home/',views.HomePage.as_view(),name='home'),
+    path('recipes/',views.RecipePage.as_view(),name='recipes'),
+    path('recipe/<int:pk>/', views.RecipeDetailView.as_view(), name='recipe_detail'),
 ]
