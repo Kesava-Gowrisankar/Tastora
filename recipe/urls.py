5f1906405cbcf787977c88cda84fdb993e760b2e
kesav <kesava@testpress.com>
2025-11-05 18:31:13 +0530

    Gemini changes

--- recipe/urls.py
+++ recipe/urls.py
@@ -4,5 +4,4 @@ app_name='recipe'
 urlpatterns=[
     path('home/',views.HomePage.as_view(),name='home'),
     path('recipes/',views.RecipePage.as_view(),name='recipes'),
-    path('recipe/<int:pk>/', views.RecipeDetailView.as_view(), name='recipe_detail'),
 ]
