4a0df003e9a1bc409fe8f332e4c1a8496f91da86
kesav <kesava@testpress.com>
2025-11-05 15:50:26 +0530

    feat: Completed Homepage

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from .models import Recipe
 
-# Create your views here.
+class HomePage(ListView):
+    model = Recipe
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        
+        context = super().get_context_data(**kwargs)
+        context['latest_recipes'] = Recipe.objects.order_by('-created_at')[:5]
+        context['popular_recipes'] = Recipe.objects.order_by('-likes')[:5]
+        return context
