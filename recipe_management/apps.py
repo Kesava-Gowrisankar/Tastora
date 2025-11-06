82a3613d04faf2ea54d05d415c0dba253e8c3f1b
kesav <kesava@testpress.com>
2025-11-06 19:05:22 +0530

    feat: Like feature

--- recipe_management/apps.py
+++ recipe_management/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class RecipeManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recipe_management'
+    def ready(self):
+        import recipe_management.signals
