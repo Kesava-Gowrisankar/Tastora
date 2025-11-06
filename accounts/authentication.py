bb9b5ce8c94a99979a8a9058a0786aa34731e90b
kesav <kesava@testpress.com>
2025-11-06 11:24:33 +0530

    feat: Login , Logout, Signup

---
+++ accounts/authentication.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+
+class Username_or_Email_Login:
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            # Try to fetch the user by username
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            try:
+                # If not found by username, try email
+                user = User.objects.get(email=username)
+            except User.DoesNotExist:
+                return None
+
+        # Check password
+        if user.check_password(password):
+            return user
+        return None
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,9 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     path('profile/',ProfileUpdateView.as_view(),name='profile'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/confirm/', LogoutConfirmView.as_view(), name='logout_confirm'),
+    path('signup/', SignupView.as_view(), name='signup'),
 ]
+
