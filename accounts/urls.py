6a595d017d6a541126c3c2ece8469fe9c9a7e99d
Kesava-Gowrisankar <kesava@testpress.in>
2025-12-07 12:02:21 +0530

    Login , Logout, SignIn feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProfileUpdateView
+from .views import ProfileUpdateView,CustomLoginView,LogoutConfirmView,SignupView
 from django.contrib.auth.views import LogoutView,LoginView
 from django.contrib.auth import views as auth_views
 
