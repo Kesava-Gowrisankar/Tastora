118bdb291394bf9a47eb9d73ed405c5d72ffca07
Kesava-Gowrisankar <kesava@testpress.in>
2025-12-07 14:51:57 +0530

    My Recipe Feature

--- recipe_management/views.py
+++ recipe_management/views.py
@@ -387,7 +387,7 @@ class AuthorRecipeListView(LoginRequiredMixin, ListView):
     context_object_name = "recipes"
 
     def get_queryset(self):
-        return Recipe.objects.filter(author=self.request.user).order_by('-created_at')
+        return Recipe.objects.filter(author=self.request.user).order_by('-created')
 
 class DeleteRecipeView(LoginRequiredMixin, View):
     template_name = "recipe_management/confirm_delete_recipe.html"
