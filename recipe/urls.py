fd2c075620bc509a34b45ab1117b05471a9dae56
Kesava-Gowrisankar <kesava@testpress.in>
2025-12-07 09:55:10 +0530

    Gemini changes

--- recipe/urls.py
+++ recipe/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 app_name='recipe'
 urlpatterns=[
-    path('home/',views.HomePage.as_view(),name='home'),
+    path('',views.HomePage.as_view(),name='home'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Recipe
 
+RECIPES_ON_HOMEPAGE = 5
 class HomePage(TemplateView):
     template_name = 'home.html'
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['latest_recipes'] = Recipe.objects.order_by('-created')[:5]
-        context['popular_recipes'] = Recipe.objects.order_by('-likes')[:5]
+        context['latest_recipes'] = Recipe.objects.order_by('-created')[:RECIPES_ON_HOMEPAGE]
+        context['popular_recipes'] = Recipe.objects.order_by('-likes')[:RECIPES_ON_HOMEPAGE]
         return context
