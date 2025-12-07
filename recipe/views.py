aabf801cda16e54021623909d09ddaa7304bbb59
Kesava-Gowrisankar <kesava@testpress.in>
2025-12-07 11:26:17 +0530

    feat: Completed Create Recipe

--- recipe/views.py
+++ recipe/views.py
@@ -10,4 +10,6 @@ class HomePage(TemplateView):
         context = super().get_context_data(**kwargs)
         context['latest_recipes'] = Recipe.objects.order_by('-created')[:RECIPES_ON_HOMEPAGE]
         context['popular_recipes'] = Recipe.objects.order_by('-likes')[:RECIPES_ON_HOMEPAGE]
-        return context
+        return context  
+
+
