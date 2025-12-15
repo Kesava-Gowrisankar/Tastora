00b18701c8fde2322bd4f917f873c4a6fce0ddc6
kesav <kesava@testpress.com>
2025-12-15 12:17:40 +0530

    Initial commit

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Collection)
-admin.site.register(models.Nutrition)
-admin.site.register(models.Recipe)
-admin.site.register(models.RecipeImage)
-admin.site.register(models.Profile)
-
 
