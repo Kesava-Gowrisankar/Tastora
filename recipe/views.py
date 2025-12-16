fad5cb243fddb9182067e0d9aa2ec145b51d7f7b
kesav <kesava@testpress.com>
2025-12-16 14:34:20 +0530

    add homepage views and urls

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Recipe
 
-# Create your views here.
+RECIPES_ON_HOMEPAGE = 5
+class HomePage(TemplateView):
+    template_name = 'home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_recipes'] = Recipe.objects.all()[:RECIPES_ON_HOMEPAGE]
+        context['popular_recipes'] = Recipe.objects.order_by('-likes')[:RECIPES_ON_HOMEPAGE]
+        return context
