03316dfc0d058c1221829c0893af7f9bc7a86deb
kesav <kesava@testpress.com>
2025-12-17 14:15:31 +0530

    feat: Profile feature

--- recipe_management/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "recipe_management"
-urlpatterns = [
-    path("create/", views.CreateRecipeView.as_view(), name="create_recipe"),
-    path("add-ingredient-form/", views.AddIngredientFormView.as_view(), name="add_ingredient_form"),
-]
