524ed8b5c877d906f0c92df4dad67e05992f011e
kesav <kesava@testpress.com>
2025-12-17 14:15:31 +0530

    feat: Profile feature

--- tastora/settings.py
+++ tastora/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,7 +43,6 @@ INSTALLED_APPS = [
     # Apps
     'accounts.apps.AccountsConfig',
     'recipe.apps.RecipeConfig',
-    'recipe_management.apps.RecipeManagementConfig',
 
     # Pip
     'widget_tweaks',
@@ -131,4 +132,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media' 
 
+LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'recipe:home'
+LOGOUT_REDIRECT_URL = 'recipe:home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+
+
