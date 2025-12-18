c249de7e3977fb9b60a08a227d9bcb765130496e
kesav <kesava@testpress.com>
2025-12-18 10:32:58 +0530

    fix: authentication

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -4,8 +4,8 @@ from django.db.models import Q
 class UsernameOrEmailLogin:
     def authenticate(self, request, username=None, password=None, **kwargs):
         user = User.objects.filter(
-            Q(username__iexact=username) |
-            Q(email__iexact=username)
+            Q(username=username) |
+            Q(email=username)
         ).first()
 
         if user and user.check_password(password):
