a608b01ee335b35a540517eb4937bd7da84d6e7c
kesav <kesava@testpress.com>
2025-12-19 14:56:16 +0530

    Edit view

--- recipe/urls.py
+++ recipe/urls.py
@@ -6,4 +6,6 @@ urlpatterns=[
     path("create/", views.CreateRecipeView.as_view(), name="create_recipe"),
     path("add-ingredient-form/", views.AddIngredientFormView.as_view(), name="add_ingredient_form"),
     path('recipe/<int:pk>/', views.RecipeDetailView.as_view(), name='recipe_detail'),
+    path('recipe/<int:pk>/edit/', views.EditRecipeView.as_view(), name='edit_recipe'),
+    
 ]
