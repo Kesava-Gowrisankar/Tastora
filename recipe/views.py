37353b667cb1c82500dbcf6430fd30332993287b
kesav <kesava@testpress.com>
2025-12-24 11:25:57 +0530

    Collection View

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,4 @@
+from typing import Collection
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views.generic import TemplateView
@@ -9,10 +10,11 @@ from django.utils.decorators import method_decorator
 from .domains import create_recipe_with_details
 from django.utils import timezone
 
-from django.views.generic import DetailView,ListView,FormView
+from django.views.generic import DetailView,ListView, FormView
+from .forms import CollectionForm
 from .domains import create_recipe_with_details, update_recipe_with_details
-from .models import Collection, Recipe, Nutrition, Ingredient, RecipeImage, RecipeLike
-from .forms import CollectionForm, IngredientFormSetClass, RecipeForm, NutritionForm, RecipeImageForm, IngredientForm
+from .models import Recipe, Nutrition, Ingredient, RecipeImage, RecipeLike
+from .forms import IngredientFormSetClass, RecipeForm, NutritionForm, RecipeImageForm, IngredientForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 RECIPES_ON_HOMEPAGE = 5
@@ -120,6 +122,7 @@ class RecipeDetailView(DetailView):
             'liked': liked,
             'now': timezone.now()
         })
+
         return context
 
 @method_decorator(login_required, name='dispatch')
@@ -175,7 +178,7 @@ class EditRecipeView(View):
         )
 
         messages.success(request, "Recipe updated successfully!")
-        return redirect('recipe:recipe_detail', pk=recipe.pk)
+        return redirect('recipe:edit_recipe', pk=recipe.pk)
     
     def forms_are_valid(self, forms):
         results = [form.is_valid() for form in forms.values() if hasattr(form, 'is_valid')]
@@ -203,9 +206,9 @@ class ToggleLikeView(LoginRequiredMixin, View):
         
         return JsonResponse({
             'liked': liked,
-            'total_likes': recipe.recipe_likes.count(),
+            'total_likes': total_likes,
         })
-
+    
 class AddToCollectionView(LoginRequiredMixin, FormView):
     template_name = 'recipe/add_to_collection.html'
     form_class = CollectionForm
